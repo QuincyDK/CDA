c329f0d3f0632b2e50361262cdc0915492092824
Quincy <kosterkont@hotmail.com>
2013-11-19 13:34:20 +0100

    test

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php	
-	include_once 'functies.php';
+	print "pushed";	include_once 'functies.php';
 ?>	
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">			
 <html>
